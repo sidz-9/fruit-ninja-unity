8c1cbad6ce2adcf20e6b48ddc776c179a4547878
sidz-9 <sid.zaveri.dev@gmail.com>
2021-03-04 01:15:33 +0530

    Added Despawnner, created game over panel, created menu scene and added collider to watermelon cut pieces(in order to despawn upon colliding with despawnner).

---
+++ Assets/Scripts/Despawnner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawnner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Destroy(col.gameObject);
+    }
+}
